c89020ad7d8866a3e1bd7b320236300968f8e941
Natnael Abay <se.natnael.abay@gmail.com>
2022-04-11 00:31:17 +0300

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import * as React from 'react';
 
-export default function HomePage() {
+import prisma from '../../lib/prisma';
+
+export default function HomePage(props) {
+  console.log(props.students)
   return <h1>Hello, world</h1>;
+}
+
+
+export async function getServerSideProps() {
+  const students = await prisma.student.findMany();
+  return {
+    props: {
+      name: 'world',
+      students
+    },
+  };
 }
